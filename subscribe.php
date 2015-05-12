f5a59708e56be7ee6ec5f0c6f0251c9fad048087
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2015-05-12 20:44:42 +0100

    mail list

--- subscribe.php
+++ subscribe.php
@@ -1,1 +1,12 @@
-{ 'success': true }
+<?php
+if (isset($_GET['email'])) {
+	$text = $_GET['email']."\n";
+	$filename = "emails.txt";
+	$fh = fopen($filename, "a");
+	$success = fwrite($fh, $text);
+	fclose($fh);
+}
+else {
+	$success = false;
+}
+?>{ 'success': <?php echo $success ? 'true' : 'false';?> }
